fe710ce2469476d572fa9a4743d1f929db338ccb
Abhishek <aaagrawal.abhishek@gmail.com>
2020-07-13 20:39:15 +0530

    Added more pattern codes

--- patterns.py
+++ patterns.py
@@ -1,8 +1,33 @@
-#first for loop is for rows
-#second for loop is for column
+# first for loop is for rows
+# second for loop is for column
+# for adding spaces before, second for loop is used and in that case third for loop is used for printing
 
 for x in range(5,0,-1):
     for y in range(0,x,1):
         print("*", end="")
+    print()
+print("-------------------------------")
+
+for x in range(1,6):
+    for y in range(5,x,-1):
+        print(" ", end="")
+    for z in range(1,x+1):
+        print(z, end = "")
+    print()
+print("-------------------------------")
+
+for x in range(1,6):
+    for y in range(5,x,-1):
+        print(" ", end="")
+    for z in range(1,x+1):
+        print("*", end = "")
+    print()
+print("-------------------------------")
+
+for x in range(1,6):
+    for y in range(5,x,-1):
+        print(" ", end = "")
+    for z in range(x):
+        print("*", end=" ")
     print()
 print("-------------------------------")
