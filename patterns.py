2f2528cd118531b1599fe41f19aafd0932200512
Abhishek Agrawal <aaagrawal.abhishek@gmail.com>
2020-07-13 20:20:47 +0530

    This file contains various patterns code

---
+++ patterns.py
@@ -0,0 +1,8 @@
+#first for loop is for rows
+#second for loop is for column
+
+for x in range(5,0,-1):
+    for y in range(0,x,1):
+        print("*", end="")
+    print()
+print("-------------------------------")
